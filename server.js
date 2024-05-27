6df8dbeac0f7fa56f6c18f2a1e855498ea7c596d
Vishnu <vishnu@c2info.com>
2024-05-27 20:13:37 +0530

    new changes

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var qr = require('qr-image');
 const qrcode = require('qrcode-terminal');
 const cls_wa_client = require('./waClient')
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
 
 const host = "0.0.0.0";
 const port = 3000;
+const wa_manage_url = "http://localhost:5003"
 
 var app = express();
 app.use(bodyParser.json());
@@ -16,18 +19,67 @@ app.get('/', function (req, res) {
     res.send("Service running");
 });
 
-app.get('/initialize/:id', function (req, res) {
-    wa_id = req.params.id;
+function create_response(b_status = true, s_status_message = "Success", a_response_data = null){
+    return {
+        status: b_status,
+        status_message: s_status_message,
+        data: a_response_data
+    }
+}
+
+async function axios_get(path){
+    url = `${wa_manage_url}${path}`;
+    try {
+        // The following line will wait until the axios request is completed
+        const response = await axios.get(url);
+        // console.log('Data:', response.data);
+        return {status: true, response: response.data};
+    } catch (error) {
+        console.error('Error:', error);
+        return {status: false, error: error};
+    }
+}
+
+function get_client(wa_id, auto_create = false){
     var wa_client = wa_clients[wa_id];
-    if (wa_client != null && wa_client != undefined){
-        res.send({ status: wa_client.status});
-        return;
+    if (wa_client != null && wa_client != undefined && wa_client.status != wa_client.STATUS_DISCONNECTED){
+        return {client: wa_client, type: "E"};
     }
 
-    wa_cls = new cls_wa_client(id=wa_id);
-    wa_clients[wa_id] = wa_cls;
+    if(!auto_create){
+        return {client: null, type: "-"};
+    }
 
-    res.send({status: wa_cls.status});
+    wa_client = new cls_wa_client(id=wa_id);
+    wa_clients[wa_id] = wa_client;
+    return {client: wa_client, type: "N"};
+}
+
+app.get('/initialize/:id', function (req, res) {
+    wa_id = req.params.id;
+    try{
+        var wa_client_request = get_client(id=wa_id, true);
+        var wa_client = wa_client_request.client;
+
+        res.send(create_response(true, "Success", {status: wa_client.status}))
+    }
+    catch(err){
+        res.send(create_response(false, err.message))
+    }
+
+    // var wa_client = wa_clients[wa_id];
+    // if (wa_client != null && wa_client != undefined && wa_client.status != wa_client.STATUS_DISCONNECTED){
+    //     res.send({ status: wa_client.status});
+    //     return;
+    // }
+
+    // try{
+    //     wa_cls = start_client(id=wa_id);
+    //     res.send(create_response(true, "Success", {status: wa_cls.status}))
+    // }
+    // catch(err){
+    //     res.send(create_response(false, err.message))
+    // }
 });
 
 app.get('/status/:id', function (req, res) {
@@ -38,27 +90,36 @@ app.get('/status/:id', function (req, res) {
         return;
     }
 
+    client_info = wa_client.client_info();
+    if (client_info == undefined){
+        client_info = null;
+    }
     if(wa_client.status == wa_client.STATUS_QR){
-        console.log(wa_client.QR);
-        qrcode.generate(wa_client.QR, { small: true });
+        // console.log(wa_client.QR);
+        // qrcode.generate(wa_client.QR, { small: true });
         var qr_svg = qr.imageSync(wa_client.QR, { type: 'png' });
         
-        res.send({ "status": wa_client.STATUS_QR, "qr_code": qr_svg.toString('base64')});
+        res.send({ status: wa_client.STATUS_QR, 
+            info: client_info,
+            qr_code: qr_svg.toString('base64')});
         return;
     }
 
-    if(wa_client.status == wa_client.STATUS_QR){
-        res.send({"status": wa_client.status, "reason": wa_client.DISCONNECT_REASON});
+    if(wa_client.status == wa_client.STATUS_DISCONNECTED){
+        res.send({status: wa_client.status, reason: wa_client.DISCONNECT_REASON,
+            info: client_info
+        });
         return;
     }
-
-    res.send({"status": wa_client.status});
+    
+    res.send({"status": wa_client.status, info: client_info});
 });
 
 app.post('/send-message/:id', async function (req, res) {
     wa_id = req.params.id;
     console.log(`sending-message with id ${wa_id}`);
-    var wa_client = wa_clients[wa_id];
+    
+    wa_client = get_client(wa_id, false).client;
     if (wa_client == null || wa_client == undefined){
         res.send({ status: "APP_NOT_FOUND", message: "Application not found"});
         return;
@@ -101,7 +162,7 @@ app.post('/send-message/:id', async function (req, res) {
     }
     
     if(mobile_user == null){
-        res.send({ status: "INVALID_MOB", message: "Invalid mobile number"});
+        res.send({ status: "INVALID_NUMBER", message: "Invalid mobile number"});
         return;
     }
 
@@ -150,9 +211,53 @@ app.get('/logout/:id', async function (req, res) {
     }
 
     await wa_client.logout();
+    await wa_client.destroy();
+    delete wa_clients[wa_id];
     res.send({ status: wa_client.status});
 });
 
+app.get('/restart/:id', async function (req, res) {
+    wa_id = req.params.id;
+    var wa_client = wa_clients[wa_id];
+    if (wa_client == null || wa_client == undefined){
+        res.send({ status: "APP_NOT_FOUND", message: "Application not found"});
+        return;
+    }
+    if(wa_client.status == wa_client.STATUS_CONNECTED){
+        await wa_client.destroy()
+    }
+
+    delete wa_clients[wa_id];
+    var wa_client_request = get_client(id=wa_id);
+    var wa_client = wa_client_request.client;
+
+    res.send({status: wa_client.status});
+});
+
+async function startup_run_wa_clients(){
+    console.log("starting wa clients");
+    client_list = await axios_get("/wa-connect/backend/wa-client/list");
+    if(!client_list.status){
+        console.log("Failed to call wa manager api");
+        console.log(client_list.error);
+        return;
+    }
+    client_list = client_list.response;
+    if(!client_list.status){
+        console.log("Failed to call wa manager api");
+        console.log(client_list.status_message);
+        return;
+    }
+
+    client_list = client_list.data;
+    console.log(client_list);
+    client_list.forEach(client => {
+        console.log(client);
+        get_client(client.client_id, true);
+    });
+}
+
 app.listen(port, host, function (){
     console.log(`application started with host:${host} port:${port}`);
+    startup_run_wa_clients();
 });
--- waClient.js
+++ waClient.js
@@ -1,15 +1,18 @@
 const { Client, RemoteAuth, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const axios = require('axios');
+
+const wa_manage_url = "http://localhost:5003";
 
 class cls_wa_client {
-    constructor(id){
+    constructor(id) {
         this.STATUS_DISCONNECTED = "DISCONNECTED";
         this.STATUS_CONNECTED = "CONNECTED";
         this.STATUS_QR = "QR";
         this.STATUS_PENDING = "STARTING";
-        this.STATUS_LOGGEDOUT = "LOGGEDOUT";
+        this.STATUS_DELETED = "DELETED";
 
         this.DISCONNECT_REASON = null;
-        
+
         this.id = id;
         this.status = this.STATUS_PENDING;
 
@@ -19,6 +22,11 @@ class cls_wa_client {
             authStrategy: new LocalAuth({ clientId: `wa_${this.id}` }),
             puppeteer: {
                 args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            },
+            webVersionCache: {
+                type: "remote",
+                remotePath:
+                    "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
             }
         });
 
@@ -27,40 +35,91 @@ class cls_wa_client {
             this.status = this.STATUS_QR;
             console.log('QR RECEIVED');
             this.QR = qr;
+            this.update_status();
         });
-        
+
         this.client.on('ready', () => {
             this.status = this.STATUS_CONNECTED;
             console.log('Client is ready!');
+            this.update_status();
         });
 
         this.client.on('disconnected', (reason) => {
             this.status = this.STATUS_DISCONNECTED;
             console.log('disconnect');
             this.DISCONNECT_REASON = reason;
+            this.update_status();
         });
 
+        this.client.on('authenticated', (session) => {
+            console.log('Authenticated');
+        });
+
+        this.client.on('auth_failure', (msg) => {
+            console.error('Authentication failure:', msg);
+        });
+
+        this.update_status();
         this.client.initialize();
         // console.log(this.client);
     }
 
-    get clientInfo(){
-        console.log(this.client);
+    client_info() {
+        var _client_info = this.client.info;
+        if (_client_info == undefined) {
+            return null;
+        }
+        _client_info = {
+            name: _client_info.pushname,
+            user: _client_info.wid.user,
+            server: _client_info.wid.server,
+            serialized: _client_info.wid._serialized
+        }
+
+        return _client_info;
     }
 
     async send_text(recipient, message) {
         return await this.client.sendMessage(recipient, message.trim());
     }
 
-    async send_document(recipient, url, caption = null){
+    async send_document(recipient, url, caption = null) {
         var media = await MessageMedia.fromUrl(url);
         return await this.client.sendMessage(recipient, media, { caption: caption });
     }
 
-    async logout(){
+    async logout() {
         await this.client.logout();
-        this.status = this.STATUS_LOGGEDOUT;
-        return;
+        this.status = this.STATUS_DELETED;
+        this.update_status();
+
+        return true;
+    }
+
+    async destroy() {
+        await this.client.destroy();
+    }
+
+    async update_status(){
+        const payload = {
+            client_id: this.id,
+            status: this.status
+        };
+
+        this.axios_post("/wa-connect/backend/wa-client/update-status", payload)
+    }
+
+    async axios_post(path, payload){
+        url = `${wa_manage_url}${path}`;
+        try {
+            // The following line will wait until the axios request is completed
+            const response = await axios.post(url, payload);
+            // console.log('Data:', response.data);
+            return {status: true, response: response.data};
+        } catch (error) {
+            console.error('Error:', error);
+            return {status: false, error: error};
+        }
     }
 }
 
